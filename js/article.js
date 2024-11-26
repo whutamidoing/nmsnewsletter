90a7346f66365cce41bad9e7f755cea8b009f540
whutamidoing <117074333+whutamidoing@users.noreply.github.com>
2024-11-26 06:35:48 +0000

    Commit again

--- js/article.js
+++ js/article.js
@@ -1,4 +1,32 @@
+//Takes a tab number and fetches a url on the corresponding number
 function changeTab(event, tabNum){
-  const title = document.querySelector('.headline');
-  
+  const article = document.querySelector('.article_wrapper');
+
+  let url = '';
+  if (tabNum == 1) {
+      url = 'article-content/tab1.html';
+  } else if (tabNum == 2) {
+      url = 'article-content/tab2.html';
+  } else if (tabNum == 3) {
+    url = 'article-content/tab3.html';
+  }  else if (tabNum == 4) {
+    url = 'article-content/tab4.html';
+  } else if (tabNum == 5) {
+    url = 'article-content/tab5.html';
+  }  else if (tabNum == 6) {
+    url = 'article-content/tab6.html';
+  }
+
+  //Load content dynamically using Ajax
+  fetch(url)
+      .then(response => response.text())
+      .then(data => {
+          article.innerHTML = data;
+      })
+        .catch(error => {
+            console.error('Error loading content:', error);
+        });
 }
+window.onload = function() {
+  changeTab(event, 1);
+};
