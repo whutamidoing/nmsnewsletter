32971fff4441afaa26ef2fe54206ef07ebf99310
whutamidoing <117074333+whutamidoing@users.noreply.github.com>
2024-11-28 05:50:35 +0000

    commit-changeday3

--- js/article.js
+++ js/article.js
@@ -1,23 +1,34 @@
 //Takes a tab number and fetches a url on the corresponding number
 function changeTab(event, tabNum){
   const article = document.querySelector('.article_wrapper');
+  const sidebar = document.querySelector('.section-content');
 
   let url = '';
+  let sidebarURL = '';
+
   if (tabNum == 1) {
-      url = 'article-content/tab1.html';
+    url = 'article-content/tab1.html';
+    sidebarURL = 'article-content/sideBar1.html';
   } else if (tabNum == 2) {
-      url = 'article-content/tab2.html';
+    url = 'article-content/tab2.html';
+    sidebarURL = 'article-content/sideBar1.html';
   } else if (tabNum == 3) {
     url = 'article-content/tab3.html';
+    sidebarURL = 'article-content/sideBar1.html';
   }  else if (tabNum == 4) {
     url = 'article-content/tab4.html';
+    sidebarURL = 'article-content/sideBar1.html';
   } else if (tabNum == 5) {
     url = 'article-content/tab5.html';
+    sidebarURL = 'article-content/sideBar1.html';
   }  else if (tabNum == 6) {
     url = 'article-content/tab6.html';
+    sidebarURL = 'article-content/sideBar1.html';
   }
 
   //Load content dynamically using Ajax
+
+  //article wrapper
   fetch(url)
       .then(response => response.text())
       .then(data => {
@@ -26,6 +37,16 @@ function changeTab(event, tabNum){
         .catch(error => {
             console.error('Error loading content:', error);
         });
+
+//sidebar
+fetch(sidebarURL)
+        .then(response => response.text())
+        .then(data => {
+            sidebar.innerHTML = data;
+        })
+          .catch(error => {
+              console.error('Error loading sidebar content:', error);
+          });
 }
 window.onload = function() {
   changeTab(event, 1);
