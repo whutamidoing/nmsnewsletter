47548bb025f70cdab37191f7e48eff6b5308e426
whutamidoing <117074333+whutamidoing@users.noreply.github.com>
2024-12-03 12:36:05 +0000

    major-changes

--- js/article.js
+++ js/article.js
@@ -2,6 +2,7 @@
 function changeTab(event, tabNum){
   const article = document.querySelector('.article_wrapper');
   const sidebar = document.querySelector('.section-content');
+  const title = document.querySelector('.section-title');
 
   let url = '';
   let sidebarURL = '';
@@ -9,21 +10,27 @@ function changeTab(event, tabNum){
   if (tabNum == 1) {
     url = 'article-content/tab1.html';
     sidebarURL = 'article-content/sideBar1.html';
+    title.innerHTML = '<h1 class="headline">COMPUTER SCIENCE AS DISCIPLINE, COMPUTING DISCIPLINES AND MAJORS</h1>';
   } else if (tabNum == 2) {
     url = 'article-content/tab2.html';
-    sidebarURL = 'article-content/sideBar1.html';
+    sidebarURL = 'article-content/sideBar2.html';
+    title.innerHTML = '<h1 class="headline">DATA AND INFORMATION, INFORMATION SYSTEMS, and DIFFERENT TYPES OF SUPPORT SYSTEMS IN INFORMATION SYSTEMS ANALYSIS/REACTION</h1>';
   } else if (tabNum == 3) {
     url = 'article-content/tab3.html';
-    sidebarURL = 'article-content/sideBar1.html';
+    sidebarURL = 'article-content/sideBar3.html';
+    title.innerHTML = '<h1 class="headline">NUMBER SYSTEMS</h1>';
   }  else if (tabNum == 4) {
     url = 'article-content/tab4.html';
-    sidebarURL = 'article-content/sideBar1.html';
+    sidebarURL = 'article-content/sideBar4.html';
+    title.innerHTML = '<h1 class="headline">Computer Hardware and Software: Their Implementations in the Different Institutions in the Community</h1>';
   } else if (tabNum == 5) {
     url = 'article-content/tab5.html';
-    sidebarURL = 'article-content/sideBar1.html';
+    sidebarURL = 'article-content/sideBar5.html';
+    title.innerHTML = '<h1 class="headline">Comparative Study on Different Types of Motherboards</h1>';
   }  else if (tabNum == 6) {
     url = 'article-content/tab6.html';
-    sidebarURL = 'article-content/sideBar1.html';
+    sidebarURL = 'article-content/sideBar6.html';
+    title.innerHTML = '<h1 class="headline">Comparative Study on Different Types of Computers</h1>';
   }
 
   //Load content dynamically using Ajax
